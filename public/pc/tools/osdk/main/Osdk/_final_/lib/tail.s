7f999ecaca23d4c7029859b714e3920c296aa79c
Dbug <mike@defence-force.org>
2023-12-31 14:05:14 +0100

    Revert "Extracted the Atari stuff out from the main repository"
    This reverts commit 1a3694680026581522a64437e3c1ce5a0e81e274.

---
+++ public/pc/tools/osdk/main/Osdk/_final_/lib/tail.s
@@ -0,0 +1,15 @@
+
+ .text
+
+;
+; This area is used as a stack.
+; it should be protected !
+;
+osdk_stack
+	.dsb 256
+
+osdk_check
+	.asc "Dbug"
+
+osdk_end 
+;.byt $FF
---
+++ users/dbug/games/Road/osdk_config.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+::
+:: Set the build paremeters
+::
+SET OSDKADDR=$600
+SET OSDKNAME=QuantumFx
+SET OSDKFILE=main profile vbl keyboard resol font table costable road
+SET OSDKDISK=%OSDKNAME%.dsk
+
